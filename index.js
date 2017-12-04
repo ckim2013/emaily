c8a7e4be6b9ddb0ba3d351ad1e4ea93439426f5c
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2017-12-04 12:40:48 -0800

    Make simple server using express

---
+++ index.js
@@ -0,0 +1,20 @@
+// import express from 'express';
+// As of the creation of the course, the nodejs runtime only has support
+// for common JS modules. Thus, import express from 'express'; wouldn't work.
+// Node.js does not have support for ES2015 modules, at least not at the
+// creation of the course. This is not a problem though for frontend React.
+const express = require('express');
+// By invoking express(), we are creating a new application that represents
+// a running express app. Inside a single node.js project, we can have multiple
+// express apps.
+const app = express();
+
+// An express server has a number of route handlers associated with it.
+// By calling app.get, we are creating a new route handler.
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+// Here, express is telling NODE to listen in on port 5000.
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
