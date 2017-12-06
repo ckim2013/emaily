0a00d7e30bdc6d1c7f19556af6140910882696df
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2017-12-05 17:20:52 -0800

    Make mlab mondodb and do initial setup with mongoose

--- index.js
+++ index.js
@@ -4,13 +4,23 @@
 // Node.js does not have support for ES2015 modules, at least not at the
 // creation of the course. This is not a problem though for frontend React.
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+// Since we are not returning anything in passport.js file and just require
+// the code to run, we can just require without assigning the require to
+// any variable.
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 // By invoking express(), we are creating a new application that represents
 // a running express app. Inside a single node.js project, we can have multiple
 // express apps.
 const app = express();
 
+// We could have required and assigned to a const, and then call the const
+// with the app but this is not necessary. We can just do below.
 require('./routes/authRoutes')(app);
 
 // Here, express is telling NODE to listen in on port 5000.
