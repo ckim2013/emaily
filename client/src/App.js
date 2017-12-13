c7a61682d453365c9fcad7cdb59a8543720acf4f
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2017-12-13 15:25:23 -0800

    Add an achor tag to the auth google sign in page and adjust package json in client

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,15 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Emaily</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h2 className='App-title'>Welcome to Emaily</h2>
         </header>
-        <p className="App-intro">
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <a href='/auth/google'>Sign in with Google</a>
       </div>
     );
   }
