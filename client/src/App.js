76e7bb499959ab7cec2aa418ce0e4c227edae054
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2017-12-14 18:30:38 -0800

    Refactor promise handling in passport.js to use await and async

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className='App'>
         <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
+          <img src={ logo } className='App-logo' alt='logo' />
           <h2 className='App-title'>Welcome to Emaily</h2>
         </header>
         <p className='App-intro'>
@@ -20,3 +20,9 @@ class App extends Component {
 }
 
 export default App;
+
+// In the development world, the proxy makes it so that when I send a request
+// from localhost:3000, the browser thinks its sending it to the same server
+// but the proxy takes that and forwards it to localhost:5000. This way, we
+// can persist our cookies in our requests and not get CORS issues. 3000 has
+// the react portion and 5000 is the backend for our dev.
--- services/passport.js
+++ services/passport.js
@@ -28,31 +28,43 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback', // We could place in actual domain but relative path is easier
-      proxy: true // By default, the strategy assumes that if our request from the browser when through a proxy, the request should no longer be https. Thus, we need to set the value of proxy to true so make the google oauth work or else we would get an error.
+      proxy: true // By default, the strategy assumes that if our request from the browser when through a proxy, the request should no longer be https. Thus, we need to set the value of proxy to true to make the google oauth work or else we would get an error.
     },
-    (accessToken, refreshToken, profile, done) => {
-      // console.log('accessToken', accessToken);
-      // console.log('refreshToken', refreshToken);
-      // console.log('profile', profile);
-      // console.log('done', done);
+    // (accessToken, refreshToken, profile, done) => {
+    //   console.log('accessToken', accessToken);
+    //   console.log('refreshToken', refreshToken);
+    //   console.log('profile', profile);
+    //   console.log('done', done);
+    //
+    //   // This query returns a promise
+    //   User.findOne({ googleId: profile.id })
+    //       .then(existingUser => {
+    //         if (existingUser) {
+    //           // We already have a record with a given profile ID.
+    //           // If we found a user, then everything is fine so we pass
+    //           // in null to the error parameter.
+    //           done(null, existingUser);
+    //         } else {
+    //           // We don't have a user record with this ID so we will make one.
+    //           // Here we create a user model instance and then save it
+    //           // (using save()) which returns a promise.
+    //           new User({ googleId: profile.id })
+    //               .save()
+    //               .then(user => done(null, user));
+    //         }
+    //       });
+    // }
 
-      // This query returns a promise
-      User.findOne({ googleId: profile.id })
-          .then(existingUser => {
-            if (existingUser) {
-              // We already have a record with a given profile ID.
-              // If we found a user, then everything is fine so we pass
-              // in null to the error parameter.
-              done(null, existingUser);
-            } else {
-              // We don't have a user record with this ID so we will make one.
-              // Here we create a user model instance and then save it
-              // (using save()) which returns a promise.
-              new User({ googleId: profile.id })
-                  .save()
-                  .then(user => done(null, user));
-            }
-          });
+    // More modern approac on handling promises
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        const user = await User({ googleId: profile.id }).save();
+        done(null, user);
+      }
     }
   )
 );
