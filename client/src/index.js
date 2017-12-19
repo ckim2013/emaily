35dc9a6d5040091c8947227c75ca79de7bdc1387
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2017-12-19 14:28:23 -0800

    Make initial wiring of redux store to react

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
 // Or we can use document.querySelector('#root')
 ReactDOM.render(
   <Provider store = { store }>
     <App />
   </Provider>,
-  document.getElementById('root'));
+  document.getElementById('root')
+);
