67dfaef9d63fdf28c6a0402fc01627b09784f3a5
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2017-12-19 14:19:06 -0800

    Make initial redux boilerplate inside index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 // Or we can use document.querySelector('#root')
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store = { store }>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
