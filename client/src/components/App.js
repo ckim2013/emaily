b5a205ad0f39e56b84fb0047675660adfe310dba
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2018-01-06 20:16:50 -0800

    Make header appear on all pages and include route for surveys new

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,19 +3,23 @@
 // capital letter. Otherwise, if the file returns a function or a series of
 // function, we will label the file with lowercase.
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const Header = () => <h2>Header</h2>
-const Dashboard = () => <h2>Dashboard</h2>
-const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
+// Dummy components
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path='/' component={ Landing } />
+          <Header />
+          <Route exact path='/' component={ Landing } />
+          <Route exact path='/surveys' component={ Dashboard } />
+          <Route path='/surveys/new' component={ SurveyNew } />
         </div>
       </BrowserRouter>
     </div>
