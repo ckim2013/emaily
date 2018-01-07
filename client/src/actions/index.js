91567850442c6921df7dd6dc69ff3696b7ee5c35
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2018-01-07 12:13:41 -0800

    Make App a class instead of a function

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,5 +2,20 @@ import axios from 'axios';
 import { FETCH_USER } from '/types';
 
 const fetchUser = () => {
-  axios.get('/api/current_user');
+  return function(dispatch) {
+    axios.get('/api/current_user')
+         .then(res => dispatch({ type: FETCH_USER, payload: res }));
+  };
 };
+
+// vs
+
+// const getUser = user => ({
+//   type: FETCH_USER,
+//   payload: user
+// });
+//
+// const fetchUser = () => dispatch => (
+//   axios.get('/api/current_user')
+//        .then(res => dispatch(getUser(res)))
+// );
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,19 +12,46 @@ const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return (
-    <div className='container'>
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path='/' component={ Landing } />
-          <Route exact path='/surveys' component={ Dashboard } />
-          <Route path='/surveys/new' component={ SurveyNew } />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+// const App = () => {
+//   return (
+//     <div className='container'>
+//       <BrowserRouter>
+//         <div>
+//           <Header />
+//           <Route exact path='/' component={ Landing } />
+//           <Route exact path='/surveys' component={ Dashboard } />
+//           <Route path='/surveys/new' component={ SurveyNew } />
+//         </div>
+//       </BrowserRouter>
+//     </div>
+//   );
+// };
+
+// Refactored to below because we want to check to
+// see if the user is logged in
+
+class App extends React.Component {
+  componentDidMount() {
+    // In future versions of react, componentwillmount might be called
+    // on multiple times automatically so by convention, didmount is the
+    // preferred location to make any type of initial ajax requests
+    
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path='/' component={ Landing } />
+            <Route exact path='/surveys' component={ Dashboard } />
+            <Route path='/surveys/new' component={ SurveyNew } />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
 export default App;
