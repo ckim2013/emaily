00ad699d9a5cfb2d100839177387fff2691fb348
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2018-01-07 16:19:38 -0800

    Added a link tag into header class that changes link depending on if the user is logged in or not

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
   renderContent() {
@@ -15,12 +16,16 @@ class Header extends React.Component {
 
   render() {
     console.log('header props', this.props);
+
+    // Using an anchor tag rather than a Link will refresh the page
+    // which doesn't look good.
     return (
       <nav>
         <div className='nav-wrapper'>
-          <a className='left brand-logo'>
+          <Link to={ this.props.auth ? '/surveys' : '/' }
+                className='left brand-logo'>
             Emaily
-          </a>
+          </Link>
           <ul className='right'>
             { this.renderContent() }
           </ul>
